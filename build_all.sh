90993cb71b82c698e395e22b0af1b91c11f584b8
Chris LaRose <cjlarose@gmail.com>
2014-12-14 00:24:28 +0000

    Add pachi to build all script

--- build_all.sh
+++ build_all.sh
@@ -4,3 +4,4 @@ docker build -t disco:0.3 - < disco/0-3.dockerfile
 docker build -t gnugo:3.6 - < gnugo/3-6.dockerfile
 docker build -t gnugo:3.8 - < gnugo/3-8.dockerfile
 docker build -t indigo:0.0 - < indigo/0-0.dockerfile
+docker build -t pachi:10.00 - < pachi/10-00.dockerfile
