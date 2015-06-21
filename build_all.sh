fe98b260eff6000f058efc555a653f01d24e26c4
Chris LaRose <cjlarose@gmail.com>
2015-06-21 14:08:06 -0700

    cjl - Update indigo version number

--- build_all.sh
+++ build_all.sh
@@ -3,6 +3,6 @@ docker build -t fuego:1.1 - < fuego/1-1.dockerfile
 docker build -t disco:0.3 - < disco/0-3.dockerfile
 docker build -t gnugo:3.6 - < gnugo/3-6.dockerfile
 docker build -t gnugo:3.8 - < gnugo/3-8.dockerfile
-docker build -t indigo:0.0 - < indigo/0-0.dockerfile
+docker build -t indigo:2006 - < indigo/2006.dockerfile
 docker build -t pachi:10.00 - < pachi/10-00.dockerfile
 docker build -t simple_go:0.4.3 - < simple_go/0-4-3.dockerfile
