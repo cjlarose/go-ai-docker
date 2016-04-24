7774af56a48d58fed71ded26aaf761e8586cde37
Chris LaRose <cjlarose@gmail.com>
2016-04-23 20:20:29 -0700

    Alphabetize build script

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 docker build -t brown:1.0 - < brown/1-0.dockerfile
-docker build -t fuego:1.1 - < fuego/1-1.dockerfile
 docker build -t disco:0.3 - < disco/0-3.dockerfile
+docker build -t fuego:1.1 - < fuego/1-1.dockerfile
 docker build -t gnugo:3.6 - < gnugo/3-6.dockerfile
 docker build -t gnugo:3.8 - < gnugo/3-8.dockerfile
 docker build -t indigo:2006 - < indigo/2006.dockerfile
